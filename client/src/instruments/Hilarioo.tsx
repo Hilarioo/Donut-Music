71e488740bdb1efef06f0897faad50f581558cd1
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2022-04-21 04:24:55 -0700

    added strings, fretboard, frets, and notes

--- client/src/instruments/Hilarioo.tsx
+++ client/src/instruments/Hilarioo.tsx
@@ -13,44 +13,68 @@ import React from "react";
 // project imports
 import { Instrument, InstrumentProps } from "../Instruments";
 
+const notes = [
+  { note: "A", duration: 0, synth: "" },
+  { note: "A#", duration: 0, synth: "" },
+  { note: "B", duration: 0, synth: "" },
+  { note: "C", duration: 0, synth: "" },
+  { note: "C#", duration: 0, synth: "" },
+  { note: "D", duration: 0, synth: "" },
+  { note: "D#", duration: 0, synth: "" },
+  { note: "E", duration: 0, synth: "" },
+  { note: "F", duration: 0, synth: "" },
+  { note: "F#", duration: 0, synth: "" },
+  { note: "G", duration: 0, synth: "" },
+  { note: "G#", duration: 0, synth: "" },
+];
+
 /** ------------------------------------------------------------------------ **
  * Contains implementation of components for Guitar.
  ** ------------------------------------------------------------------------ */
 
 const Guitar = () => {
   return (
-    <>
+    <div className='guitar-container'>
       <div className='guitar'>
-        <div className='g-string'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((i) => (
-            <div key={`g${i}`} className='fret'>
-              {i}
+        <div className='string g-str'>
+          {notes
+            .slice(-2)
+            .concat(notes)
+            .map((i) => (
+              <div key={i.note} className='fret'>
+                <span className='fret-key'>{i.note}</span>
+              </div>
+            ))}
+        </div>
+        <div className='string d-str'>
+          {notes
+            .slice(5)
+            .concat(notes.slice(0, 7))
+            .map((i) => (
+              <div key={i.note} className='fret'>
+                <span className='fret-key'>{i.note}</span>
+              </div>
+            ))}
+        </div>
+        <div className='string a-str'>
+          {notes.concat(notes.slice(0, 2)).map((i) => (
+            <div key={i.note} className='fret'>
+              <span className='fret-key'>{i.note}</span>
             </div>
           ))}
         </div>
-        <div className='d-string'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((i) => (
-            <div key={`d${i}`} className='fret'>
-              {i}
-            </div>
-          ))}
-        </div>
-        <div className='a-string'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((i) => (
-            <div key={`a${i}`} className='fret'>
-              {i}
-            </div>
-          ))}
-        </div>
-        <div className='e-string'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((i) => (
-            <div key={`e${i}`} className='fret'>
-              {i}
-            </div>
-          ))}
+        <div className='string e-str'>
+          {notes
+            .slice(-5)
+            .concat(notes.slice(0, -3))
+            .map((i) => (
+              <div key={i.note} className='fret'>
+                <span className='fret-key'>{i.note}</span>
+              </div>
+            ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
