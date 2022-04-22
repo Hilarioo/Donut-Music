70423f8cb901f345e0f57ec5392344f6e9f6e50e
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2022-04-22 14:51:38 -0700

    string ketframe animation for bass guitar cords progressed

--- client/src/instruments/Hilarioo.tsx
+++ client/src/instruments/Hilarioo.tsx
@@ -8,7 +8,7 @@
 import * as Tone from "tone";
 import classNames from "classnames";
 import { List, Range } from "immutable";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // project imports
 import { Instrument, InstrumentProps } from "../Instruments";
@@ -28,17 +28,16 @@ const notes = [
   { note: "G#", duration: 0, synth: "" },
 ];
 
-// on click vibrate string(s)
-const vibrateStrings = (e: any) => {
-  // [TEST] outputs fret clicked and any after
-  console.log(e.target.id);
-};
-
 /** ------------------------------------------------------------------------ **
  * Contains implementation of components for Guitar.
  ** ------------------------------------------------------------------------ */
 
 const Guitar = () => {
+  const [cord01, setCord01] = useState(0);
+  const [cord02, setCord02] = useState(0);
+  const [cord03, setCord03] = useState(0);
+  const [cord04, setCord04] = useState(0);
+
   return (
     <div className='guitar-container'>
       <div className='guitar'>
@@ -51,10 +50,14 @@ const Guitar = () => {
                 key={`g${i}-` + v.note}
                 id={`g${i}-` + v.note}
                 className='fret'
-                onClick={(e) => {
-                  vibrateStrings(e);
-                }}>
-                <div className='string g-str' id={`g${i}-` + v.note}></div>
+                onClick={() => setCord01(1)}
+                onAnimationEnd={() => setCord01(0)}>
+                <div
+                  toggle-animation={cord01}
+                  className={`string g-str ${
+                    !cord01 ? "" : "vshake-animation"
+                  }`}
+                  id={`g${i}-` + v.note}></div>
                 <span className='fret-key' id={`g${i}-` + v.note}>
                   {v.note}
                 </span>
@@ -70,10 +73,14 @@ const Guitar = () => {
                 key={`d${i}-` + v.note}
                 id={`d${i}-` + v.note}
                 className='fret'
-                onClick={(e) => {
-                  vibrateStrings(e);
-                }}>
-                <div className='string d-str' id={`d${i}-` + v.note}></div>
+                onClick={() => setCord02(1)}
+                onAnimationEnd={() => setCord02(0)}>
+                <div
+                  toggle-animation={cord02}
+                  className={`string d-str ${
+                    !cord02 ? "" : "vshake-animation"
+                  }`}
+                  id={`d${i}-` + v.note}></div>
                 <div className='fret-key' id={`d${i}-` + v.note}>
                   {v.note}
                 </div>
@@ -86,10 +93,12 @@ const Guitar = () => {
               key={`a${i}-` + v.note}
               id={`a${i}-` + v.note}
               className='fret'
-              onClick={(e) => {
-                vibrateStrings(e);
-              }}>
-              <div className='string a-str' id={`a${i}-` + v.note}></div>
+              onClick={() => setCord03(1)}
+              onAnimationEnd={() => setCord03(0)}>
+              <div
+                toggle-animation={cord03}
+                className={`string a-str ${!cord03 ? "" : "vshake-animation"}`}
+                id={`a${i}-` + v.note}></div>
               <span className='fret-key' id={`a${i}-` + v.note}>
                 {v.note}
               </span>
@@ -105,10 +114,14 @@ const Guitar = () => {
                 key={`e${i}-` + v.note}
                 id={`e${i}-` + v.note}
                 className='fret'
-                onClick={(e) => {
-                  vibrateStrings(e);
-                }}>
-                <div className='string e-str' id={`e${i}-` + v.note}></div>
+                onClick={() => setCord04(1)}
+                onAnimationEnd={() => setCord04(0)}>
+                <div
+                  toggle-animation={cord04}
+                  className={`string e-str ${
+                    !cord04 ? "" : "vshake-animation"
+                  }`}
+                  id={`e${i}-` + v.note}></div>
                 <span className='fret-key' id={`e${i}-` + v.note}>
                   {v.note}
                 </span>
