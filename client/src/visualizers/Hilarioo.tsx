5e7d3b4790dea79675dea5291d830c7449372833
Jose H Gonzalez <40965405+Hilarioo@users.noreply.github.com>
2022-05-03 13:40:33 -0700

    3D visual added which rotates based on time, and object shakes based on waveform input -1 to 1

--- client/src/visualizers/Hilarioo.tsx
+++ client/src/visualizers/Hilarioo.tsx
@@ -5,23 +5,49 @@
 */
 
 // 3rd party library imports
+import { Box } from "@mui/system";
 import P5 from "p5";
 import * as Tone from "tone";
 
 // project imports
 import { Visualizer } from "../Visualizers";
 
+let duration = 0;
+
+const rotateBox = (
+  p5: P5,
+  width: number,
+  height: number,
+  depth: number,
+  time: number
+) => {
+  p5.normalMaterial();
+  p5.rotateX(duration);
+  p5.rotateY(duration);
+  p5.rotateZ(duration / 2);
+  // box([width], [Height], [depth], [detailX], [detailY])
+  p5.box(width, height, depth);
+  if (time !== 0) duration += 0.0001;
+  // console.log("duration: " + duration);
+};
+
 const HVisualizer = (p5: P5, analyzer: Tone.Analyser) => {
   const width = window.innerWidth;
   const height = window.innerHeight / 2;
+
   p5.background(42, 46, 55);
 
+  // trap(p5, 500, 200, 100, 4);
+
   const values = analyzer.getValue();
-  p5.fill(0, 128, 128);
+  console.log("values: " + values);
+
   for (let i = 0; i < values.length; i++) {
     const amp = values[i] as number;
-    const d = p5.map(amp, 0, 0.3, 100, 1);
-    p5.ellipse(width / 2, height / 2, d, d);
+    // map(value, start1, stop1, start2, stop2, [withinBounds])
+    const d = p5.map(amp, 0, 1, -100, 100);
+    // webgl ( Waveform)
+    rotateBox(p5, d, d, 100, amp);
   }
 };
 
