feb9545d2f44e71db7739cd0cfa5d0de5840eac1
justdio <justindiones@gmail.com>
2022-05-04 17:27:00 -0700

    Added bars to visualizer

--- client/src/visualizers/JustDio.tsx
+++ client/src/visualizers/JustDio.tsx
@@ -6,6 +6,7 @@ import * as Tone from 'tone';
 
 export const JustDioVisualizer = new Visualizer('@JustDio: Visualizer', 
 (p5: P5, analyzer: Tone.Analyser) => {
+    // randomColor function to change color on click
     function randomColor(){
         let letters = '0123456789ABCEDF';
         let color = '#';
@@ -23,6 +24,7 @@ export const JustDioVisualizer = new Visualizer('@JustDio: Visualizer',
     p5.strokeWeight(dim * 0.01);
     p5.stroke(255, 255, 255, 255);
     
+    // change colors on mouse click
     let value = 0;
     p5.mouseClicked = () => { 
         if (value === 0){
@@ -34,18 +36,28 @@ export const JustDioVisualizer = new Visualizer('@JustDio: Visualizer',
     }
 
     let values = analyzer.getValue();
-    p5.angleMode("degrees")
+    p5.angleMode("degrees");
   
     p5.translate(width/2.5, height/2)
     for (let t = -1; t <= 1; t +=2){
+        // create circle
         p5.beginShape();
         for (let i = 0 ; i <= 180; i++) {
             let amplitude = values[i] as number;
-            let radius = p5.map(amplitude, 0, 1, height/3, 0);
+            let radius = p5.map(amplitude, 0, 1, height/4, 0);
             let x = radius * p5.sin(i) * t;
             let y = radius * p5.cos(i);
-        p5.vertex(x , y );
+            p5.vertex(x, y);
         }
         p5.endShape();
+    }
+
+    for (let i = 0; i <= 1000; i++){
+        let space = width/56;
+        let amplitude = values[i] as number;
+        p5.stroke(255);
+        let y = p5.map(amplitude, -1, 1, height, 0);
+        // p5.line(i * space, height, i * space, y);
+        p5.rect(width - (i*space), y, space, height-y)
     }
 });
