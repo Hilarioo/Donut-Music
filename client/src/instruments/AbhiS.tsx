6f6455bdc72a8ab8b5687733cc9fd583255003ba
Abhijai Singh <abhijai@hotmail.com>
2022-05-07 20:23:10 -0700

    Final Edits to Violin Instrument

--- client/src/instruments/AbhiS.tsx
+++ client/src/instruments/AbhiS.tsx
@@ -1,35 +1,40 @@
-// 3rd party library imports
+//External library imports
 import * as Tone from 'tone';
 import classNames from 'classnames';
-import {List, Range} from 'immutable';
+import { List, Range } from 'immutable';
 import React, {useEffect} from 'react';
 
-// Importing Instrument, OscillatorOptions, and RecursivePartial
+// Importing Instrument, OscillatorOptions, and RecursivePartial and Material UI
+
 import {Instrument, InstrumentProps} from '../Instruments';
 import {OmniOscillatorOptions} from "tone/Tone/source/oscillator/OscillatorInterface";
+import Button from "@mui/material/Button";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 import {RecursivePartial} from "tone/Tone/core/util/Interface";
 
-//Importing Violin B&G Template
-
+//Importing Violin B&W Template
 import image from './../img/ViolinTemplate.png';
 
-/** ------------------------------------------------------------------------ **
- * Contains implementation of components for Violin.
- ** ------------------------------------------------------------------------ */
 
+/** ------------------------------------------------------------------------ **
+ * Implementation of components for Violin.
+ ** ------------------------------------------------------------------------ */
+//Violin interface attributes definition.
 interface ViolinKeyProps {
-  note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
-  duration?: string; 
+  note: string; // Incorporates C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
+  duration?: string; //
   synth?: Tone.Synth; // Contains library code for making sound
-  UpDmn: number;
+  UpDmn: number; //Upper Key Dimension value.
   minor?: boolean; // True if minor key, false if major key
-  octave: number;
-  index: number;  
-  LatDmn: number; 
+  octave: number; // octave value
+  index: number;  // octave + index together give a location for the piano key
+  LatDmn: number; //Lateral Key Dimension value
   orientation: string; 
 }
 
-export function ViolinKey({note, synth, minor, UpDmn, index, LatDmn, orientation}: ViolinKeyProps): JSX.Element {
+export function ViolinStringKey({note, synth, minor, UpDmn, index, LatDmn, orientation}: ViolinKeyProps): JSX.Element {
   return (
     <div
       style={{
@@ -37,7 +42,7 @@ export function ViolinKey({note, synth, minor, UpDmn, index, LatDmn, orientation
       }}>
       <div
         onMouseDown={() => synth?.triggerAttack(`${note}`)} 
-        onMouseUp={() => synth?.triggerRelease('+0.1')}
+        onMouseUp={() => synth?.triggerRelease('+0.9')}
         className={classNames('ba pointer absolute dim')}
         style={{
           top: UpDmn,
@@ -46,15 +51,15 @@ export function ViolinKey({note, synth, minor, UpDmn, index, LatDmn, orientation
           zIndex: 0,
           marginLeft: minor ? '0.25rem' : 0,
           background: "#C8C9C5",
-          height: '15px',
-          width: "26px",
-          borderRadius: "13px"
+          height: '11px',
+          width: "23px",
+          borderRadius: "20px"
         }}>
       </div>
     </div>
   );
 }
-
+//Defining the main Violin Keys function
 function Violin({synth, setSynth}: InstrumentProps): JSX.Element {
   const keys = List([
     { note: 'C', idx: 0 },
@@ -71,11 +76,13 @@ function Violin({synth, setSynth}: InstrumentProps): JSX.Element {
     { note: 'B', idx: 6 },
   ]);
 
+
+//Defining Oscillator function
   const setOscillator = () => {
     setSynth(oldSynth => {
       oldSynth.disconnect();
       return new Tone.Synth({
-        "volume": 5,
+        "volume": 4,
         "detune": 0,
         "portamento": 0,
         "envelope": {
@@ -102,90 +109,21 @@ function Violin({synth, setSynth}: InstrumentProps): JSX.Element {
       }).toDestination();}
     );
   };
-  useEffect(setOscillator, [setSynth]);
+  
+   useEffect(setOscillator, [setSynth]);
+//Defining window size, importing image and key octave + orientation and dimensions
+//Prefer to open in Chrome as opposed to other browsers due to scaling issues.
 
   return (
     <div className="pv4">
       <div className={"image"}>
         <img src={image} alt={"Violin"} style={{
           position: 'absolute',
-          top: window.innerHeight/16,
-          left: window.innerWidth/8
+          top: window.innerHeight/25,
+          left: window.innerWidth/10
         }}/>
-      </div>
-      <div className="relative dib h4 w-100 ml4">
-        {Range(3, 4).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave+1}`;
-            if (isMinor) {
-              return null
-            } 
-            return (
-              <ViolinKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-                UpDmn={125}
-				minor={isMinor}
-                octave={octave+1}
-                index={(octave - 2) * 7 + key.idx}                
-                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+32}
-                orientation={"rotate(1.25deg)"}
-              />
-            );
-          }),
-        )}
-      </div>
-      <div className="relative dib h4 w-100 ml4">
-        {Range(4, 5).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave+1}`;
-            if (isMinor) {
-              return null
-            }
-            return (
-              <ViolinKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-				UpDmn={120}
-                minor={isMinor}
-                octave={octave+1}
-                index={(octave - 2) * 7 + key.idx}                
-                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+18}
-                orientation={"rotate(1.5deg)"}
-              />
-            );
-          }),
-        )}
-      </div>
-      <div className="relative dib h4 w-100 ml4">
-        {Range(5, 6).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave+1}`;
-            if (isMinor) {
-              return null
-            }
-            return (
-              <ViolinKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-				UpDmn={40}
-                minor={isMinor}
-                octave={octave+1}
-                index={(octave - 2) * 7 + key.idx}                
-                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+3.5}
-                orientation={"rotate(0deg)"}
-              />
-            );
-          }),
-        )}
-      </div>
-      <div className="relative dib h4 w-100 ml4">
+		
+		<div className="E String Keys relative dib h4 w-100 ml4"> 
         {Range(6, 7).map(octave =>
           keys.map(key => {
             const isMinor = key.note.indexOf('b') !== -1;
@@ -194,22 +132,99 @@ function Violin({synth, setSynth}: InstrumentProps): JSX.Element {
               return null
             }
             return (
-              <ViolinKey
-                key={note} //react key
+              <ViolinStringKey
+                key={note} 
                 note={note}
                 synth={synth}
-				UpDmn={-25}
+				UpDmn={130}
                 minor={isMinor}
                 octave={octave+1}
                 index={(octave - 2) * 7 + key.idx}
                 LatDmn={(((octave - 2) * 7 + key.idx) * 2)-10.5}
+                orientation={"rotate(2.5deg)"}
+              />
+            );
+          }),
+        )}
+      </div>
+	  
+      <div className="A String Keys relative dib h4 w-100 ml4"> 
+        {Range(5, 6).map(octave =>
+          keys.map(key => {
+            const isMinor = key.note.indexOf('b') !== -1;
+            const note = `${key.note}${octave+1}`;
+            if (isMinor) {
+              return null
+            }
+            return (
+              <ViolinStringKey
+                key={note} 
+                note={note}
+                synth={synth}
+				UpDmn={117}
+                minor={isMinor}
+                octave={octave+1}
+                index={(octave - 2) * 7 + key.idx}                
+                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+3.5}
+                orientation={"rotate(1deg)"}
+              />
+            );
+          }),
+        )}
+      </div>
+	  
+      <div className="D String Keys relative dib h4 w-100 ml4"> 
+        {Range(4, 5).map(octave =>
+          keys.map(key => {
+            const isMinor = key.note.indexOf('b') !== -1;
+            const note = `${key.note}${octave+1}`;
+            if (isMinor) {
+              return null
+            }
+            return (
+              <ViolinStringKey
+                key={note} //react key
+                note={note}
+                synth={synth}
+				UpDmn={9}
+                minor={isMinor}
+                octave={octave+1}
+                index={(octave - 2) * 7 + key.idx}                
+                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+17}
+                orientation={"rotate(-0.5deg)"}
+              />
+            );
+          }),
+        )}
+      </div>
+     
+      
+     </div>
+      <div className="G String Keys relative dib h4 w-100 ml4"> 
+        {Range(3, 4).map(octave =>
+          keys.map(key => {
+            const isMinor = key.note.indexOf('b') !== -1;
+            const note = `${key.note}${octave+1}`;
+            if (isMinor) {
+              return null
+            } 
+            return (
+              <ViolinStringKey
+                key={note} //react key
+                note={note}
+                synth={synth}
+                UpDmn={-50}
+				minor={isMinor}
+                octave={octave+1}
+                index={(octave - 2) * 7 + key.idx}                
+                LatDmn={(((octave - 2) * 7 + key.idx) * 2)+31}
                 orientation={"rotate(-1.5deg)"}
               />
             );
           }),
         )}
       </div>
-    </div>
+	</div>
   );
 }
 
--- client/src/visualizers/AbhiS.tsx
+++ client/src/visualizers/AbhiS.tsx
@@ -1,3 +1,72 @@
-import { Visualizer } from "../Visualizers";
+// 3rd party library imports
+import P5 from 'p5';
+import * as Tone from 'tone';
 
-export const AbhiSVisualizer = new Visualizer('@AbhiS: Visualizer', () => {});
+// project imports
+import { Visualizer } from '../Visualizers';
+
+var start = 0;
+
+export const AbhiSVisualizer = new Visualizer(
+  '@AbhiS',
+  (p5: P5, analyzer: Tone.Analyser) => {
+    const width = window.innerWidth;
+    const height = window.innerHeight / 2;
+
+   // p5.background(10, 10, 25);
+
+p5.background(0, 0, 0, 255);
+  //  p5.strokeWeight(dim * 0.01);
+  //  p5.stroke(255, 255, 255, 255);
+    // p5.noStroke();
+    // p5.stroke(255, 255, 255, 255);
+    // p5.noFill();
+
+    //center circle
+    p5.translate(width / 2.5, height / 2);
+    p5.noiseDetail(2, 1);
+	
+
+    const values = analyzer.getValue();
+    p5.beginShape();
+
+    var space = 1
+
+    for (let i = 0; i < 360; i += space) {
+      var amplitude = values[i] as number;
+      var my_amp = Math.abs(amplitude)
+
+      // gives constant movement
+      var xoff = p5.map(Math.cos(i), 0, 1, 0, my_amp * 10)
+      var yoff = p5.map(Math.sin(i), 0, 1, 0, my_amp * 10)
+      var n = p5.noise(xoff + start, yoff + start) * (my_amp * 5)
+
+      // rectangle dims
+      var h = p5.map(n, 0, 1, 0, 200)
+      var w = 3
+
+      // colors
+      var r = p5.map(Math.sin(i), 0, 1, 100, my_amp * 150 + 147)
+      var g = p5.map(i / 4, -100, 100, 0, my_amp * 150 + 112)
+      var b = p5.map(n, 0, 1, 175, my_amp * 150 + 219)
+      p5.stroke(r, g, b);
+      p5.fill(r, g, b);
+
+      // drawing the circle visual
+      p5.rotate(space);
+      p5.rect(90, 0, h, w);
+
+      // creation of a particle.
+      var x = p5.random(0, 75)+(my_amp*p5.random(0, width*2));
+      var y = p5.random(0, 75)+(my_amp*p5.random(0, height*2));
+      var size = p5.random(1, 10);
+      p5.circle(x, y, size);
+
+    }
+    start += 0.03;
+
+    p5.endShape();
+
+  },
+  
+);
