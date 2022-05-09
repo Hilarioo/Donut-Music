06380292bf65355ad57e3e2ab6c1e20efda82ca6
Abhijai Singh <abhijai@hotmail.com>
2022-05-09 01:12:57 -0700

    visualizer edits

--- client/src/visualizers/AbhiS.tsx
+++ client/src/visualizers/AbhiS.tsx
@@ -24,7 +24,7 @@ p5.background(0, 0, 0, 255);
 
     //center circle
     p5.translate(width / 2.5, height / 2);
-    p5.noiseDetail(2, 1);
+    p5.noiseDetail(3,2);
 	
 
     const values = analyzer.getValue();
@@ -43,7 +43,7 @@ p5.background(0, 0, 0, 255);
 
       // rectangle dims
       var h = p5.map(n, 0, 1, 0, 200)
-      var w = 3
+      var w = 4
 
       // colors
       var r = p5.map(Math.sin(i), 0, 1, 100, my_amp * 150 + 147)
@@ -54,16 +54,26 @@ p5.background(0, 0, 0, 255);
 
       // drawing the circle visual
       p5.rotate(space);
-      p5.rect(90, 0, h, w);
+      p5.rect(170, 50, h, w);
 
       // creation of a particle.
-      var x = p5.random(0, 75)+(my_amp*p5.random(0, width*2));
-      var y = p5.random(0, 75)+(my_amp*p5.random(0, height*2));
+      var x = p5.random(0, 30)+(my_amp*p5.random(0, width*2));
+      var y = p5.random(0, 30)+(my_amp*p5.random(0, height*2));
       var size = p5.random(1, 10);
       p5.circle(x, y, size);
 
     }
-    start += 0.03;
+	
+   for (let i = 0; i <= 700; i++){
+        let space = width/60;
+        let my_amp = values[i] as number;
+        p5.stroke(125);
+        let y = p5.map(my_amp, -1, 1, height, 0);
+        // p5.line(i * space, height, i * space, y);
+        p5.rect(width - (i*space), y, space, height-y)
+    }
+	
+	start += 0.03;
 
     p5.endShape();
 
