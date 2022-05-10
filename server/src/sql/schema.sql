c03559a9e7b9d67832c5c0a334b678dfef400634
Abhijai Singh <abhijai@hotmail.com>
2022-05-10 03:03:14 -0500

    adding beautiful zedd keynotes

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,4 +9,5 @@ VALUES
 	(1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4'),
 	(2, 'Hilarioo Song', 'F4 F4 F4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4'),
 	(3, 'JustDio Song: Michael Jackson - Smooth Criminal', 'A4 A4 A4 A4 E4 A4 B4 B4 A4 B4 C5 C5 B4 C5 B4 G4 A4'),
-	(4, 'Rui Huang Song: Mary Had A little Lamb', 'E4 D4 C4 D4 E4 E4 E4 D4 D4 D4 D4 E4 E4 E4 D4 C4 D4 E4 E4 E4 E4 D4 D4 E4 D4 C4');
+	(4, 'Rui Huang Song: Mary Had A little Lamb', 'E4 D4 C4 D4 E4 E4 E4 D4 D4 D4 D4 E4 E4 E4 D4 C4 D4 E4 E4 E4 E4 D4 D4 E4 D4 C4'),
+	(5, 'Abhijai Singh Song: Beautiful Now - Zedd', 'B4 B4 D4 B4 G4 G4 F4 B4 B4 D4 B4 G4 G4 F4 B4 B4 D4 B4 A4 A4 F4 D4 C4');
